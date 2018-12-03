90fc81be734cd96b696997aaef40897b9dfb484c
xubo016 <41979134+xubo016@users.noreply.github.com>
2018-12-03 12:01:51 +0800

    日历有问题

---
+++ application/admin/controller/Blank.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\admin\controller;
+class Blank
+{
+  public function index(){
+    return view();
+  }
+}
+?>
---
+++ application/admin/controller/Calendar.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\admin\controller;
+class Calendar
+{
+  public function index(){
+    return view();
+  }
+}
+?>
